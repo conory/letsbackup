733ab1bfd8f1ad56652ccb2998ccea7f11a21e30
conory <conory@conory.com>
2024-01-19 00:43:30 +0900

    Clean up code

--- letsbackup.sh
+++ letsbackup.sh
@@ -1,39 +1,37 @@
 #!/usr/bin/env bash
 function backup
 {
-	# backup
-	msg "Initialization completed. \n"
+	echo -e "\e[33mStart backup.\e[0m"
 	for target_path in `find /home -mindepth 1 -maxdepth 1 -type d`; do
 		_backupFile $target_path
 	done
 	_backupFile /var/log
 	_backupMysql
 	
-	# check
+	# Check if the backup was successful
 	if [[ ! -d $dir_backup_file && ! -d $dir_backup_mysql ]]; then
-		msg "\e[31mBackup failed!\e[0m \n"
+		echo -e "\e[31mBackup failed.\e[0m"
 		return
 	fi
 	
-	# upload to remote storage
-	msg "Uploading to remote storage \n"
+	# Uploading to remote storage...
+	echo -e "\e[33mUploading to remote storage...\e[0m"
 	rclone copy $dir_storage $rclone_remote_prefix --progress --transfers 1 --buffer-size 0M
 	
-	# update timestamps of working directories for prevent deleted
+	# Update timestamps of working directories for prevent deleted
 	find $dir_storage/$date_month -type d | xargs -r touch
 	
-	# delete expired backup files on local storage
+	# Delete expired backup files on local storage
 	find $dir_storage -mindepth 1 -mtime +$local_expire_days | xargs -r rm -rf
 	
-	# delete expired backup files on remote storage
+	# Deleting expired backup files on remote storage...
 	if [[ -d $dir_snap/$date_expire_month ]]; then
-		msg "Deleting expired backup files on remote storage \n"
+		echo -e "\e[33mDeleting expired backup files on remote storage...\e[0m"
 		rclone delete $rclone_remote_prefix/$date_expire_month --progress > /dev/null 2>&1
 		rm -rf $dir_snap/$date_expire_month
 	fi
 	
-	# end
-	msg "Backup completed! \n"
+	echo -e "\e[33mCompleted.\e[0m"
 }
 
 function restore
@@ -42,38 +40,36 @@ function restore
 	local _remote_path=$2
 	local _restore_path=$_local_path/restore
 	
-	# get backup files from remote storage
+	# Downloading backup files from remote storage...
 	if [[ -n $_remote_path ]]; then
 		if compgen -G "$_local_path/*" > /dev/null; then
-			echo -e "\e[31mWarning: Existing files in the target path will be deleted if continue.\e[0m"
+			echo -e "\e[31mWarning: If you continue, existing files in the local path will be deleted.\e[0m"
 			read -p "Y/n> " _whether
 			if [[ $_whether != "Y" ]]; then
 				return
 			fi
 		fi
-		msg "Getting backup files from remote storage. \n"
+		echo -e "\e[33mDownloading backup files from remote storage...\e[0m"
 		rm -rf $_local_path && mkdir -p $_local_path
 		rclone copy $rclone_remote_prefix$_remote_path $_local_path --progress
 	fi
 	
-	# check
+	# Check if backup files exist
 	local _backup_files=`find $_local_path -maxdepth 1 -type f -name "*.tgz" | sort`
 	if [[ -z $_backup_files ]]; then
-		msg "\e[31mtgz backup files not exists in the target path\e[0m \n"
+		echo -e "\e[31mNo backup files in the local path.\e[0m"
 		return
-	elif [[ -n $_remote_path ]]; then
-		msg "Download completed! \n"
 	fi
 	rm -rf $_restore_path && mkdir -p $_restore_path
 	
-	# restoring
-	msg "Restoring ... \n"
+	# Restoring...
+	echo -e "\e[33mRestoring...\e[0m"
 	for tgz_file in $_backup_files; do
+		echo "Unpacking $(basename $tgz_file)"
 		tar -zxGf $tgz_file -C $_restore_path
 	done
 	
-	# end
-	msg "Restore completed! \n"
+	echo -e "\e[33mCompleted.\e[0m"
 }
 
 function _backupFile
@@ -84,17 +80,17 @@ function _backupFile
 	local _backup_file_name=${1//\//.}
 	_backup_file_name=${_backup_file_name:1}
 	
-	# check already snap
+	# Check if the snap file already exists
 	if [[ -f $_file_snap ]]; then
-		msg "Skipping already exist backup of $1 \n"
+		echo "Already backuped $1 skipped."
 		return
 	fi
 	
-	# making snap directory
+	# Create the snap directory
 	if [[ ! -d $_dir_snap ]]; then
 		mkdir -p $_dir_snap
 		local _base=".base"
-	# copy previous snap
+	# Copy the previous snap file for incremental backup
 	else
 		local _previous_snap=`find $_dir_snap -maxdepth 1 -type f -name "*.snap" | sort | tail -n 1`
 		if [[ -n $_previous_snap ]]; then
@@ -104,11 +100,11 @@ function _backupFile
 		fi
 	fi
 	
-	# making file directory
+	# Create the file directory
 	mkdir -p $_dir_file
 	
-	# packing
-	msg "Packing file $1 ..."
+	# Packing the path ...
+	echo -en "\e[33mPacking $1 ...\e[0m"
 	tar -g $_file_snap -zcf $_dir_file/$_backup_file_name.$date_time$_base.tgz --atime-preserve=system \
 		--exclude=$letsbackup_path \
 		--exclude=files/attach/chunks \
@@ -121,8 +117,7 @@ function _backupFile
 		--exclude=files/member_extra_info/point \
 		--exclude=files/tmp \
 	$1 > /dev/null 2>&1
-	
-	msg "completed"
+	echo -e "\e[32m Completed \e[0m"
 }
 
 function _backupMysql
@@ -131,39 +126,29 @@ function _backupMysql
 		mysql_auth_option="-h $mysql_host -u $mysql_user -p$mysql_password"
 	fi
 	
-	# get database lists
+	# Get database list
 	local _database_list=`mariadb $mysql_auth_option -e "SHOW DATABASES;" --skip-column-names | grep -Ev "(information_schema|performance_schema|mysql|phpmyadmin|sys)"`
 	if [[ $? != 0 ]]; then
-		msg "\e[31mCannot access or no database\e[0m \n"
+		echo "No databases or are inaccessible."
 		return
 	fi
 	
 	for database_name in $_database_list; do
 		local _dir_database=$dir_backup_mysql/$database_name
 		
-		# making database directory
+		# Create the database directory
 		mkdir -p $_dir_database
 		
-		# exporting
-		msg "Exporting $database_name database to file ..."
+		# Exporting the database to file ...
+		echo -en "\e[33mExporting $database_name database to file ...\e[0m"
 		mariadb-dump $mysql_auth_option --opt --single-transaction -e $database_name | gzip > $_dir_database/$database_name.$date_time.sql.gz
-		msg "completed"
+		echo -e "\e[32m Completed \e[0m"
 	done
 }
 
-function msg
-{
-	if [[ $1 == "completed" ]]; then
-		echo -e "\e[32m completed \e[0m"
-	elif [[ $1 == "failed" ]]; then
-		echo -e "\e[31m failed \e[0m"
-	else
-		echo -ne "\e[33m$1\e[0m"
-	fi
-}
 
 
-msg "Let's backup! script ver 1.2.0 \n"
+echo -e "\e[33mLet's Backup! script ver 1.2.0\e[0m"
 
 command=${*:$OPTIND:1}
 arg1=${*:$OPTIND+1:1}
@@ -195,7 +180,7 @@ EOF
 		read -s -p "MariaDB password: " mysql_password
 		echo ""
 		if [[ -z $mysql_user ]]; then
-			echo -e "\e[31mThere is no input.\e[0m"
+			echo "No input."
 			exit
 		fi
 		mysql_auth_option="-h localhost -u $mysql_user -p$mysql_password"
@@ -211,7 +196,7 @@ Important: Must be the same "remote name of rclone config" and "bucket name of r
 EOF
 	read -p "string> " rclone_remote_name
 	if [[ -z $rclone_remote_name ]]; then
-		echo -e "\e[31m\nThere is no input.\e[0m"
+		echo "No input."
 		exit
 	fi
 	if rclone lsf $rclone_remote_name:$rclone_remote_name > /dev/null 2>&1; then
@@ -255,26 +240,26 @@ fi
 ((remote_expire_months++))
 ((local_expire_days--))
 
-# set date
+# Set date
 date_month=`date +%Y%m`
 date_time=`date +%Y%m%d%H%M%S`
 date_expire_month=`date +%Y%m -d "$remote_expire_months month ago"`
 
-# set directory
+# Set directory
 mkdir -p $letsbackup_path && chmod 700 $letsbackup_path
 dir_storage=$letsbackup_path/storage
 dir_snap=$letsbackup_path/snap
 dir_backup_file=$dir_storage/$date_month/file
 dir_backup_mysql=$dir_storage/$date_month/mysql
 
-# set parameters
+# Set parameters
 case $command in
 	backup)
 		backup
 	;;
 	restore)
 		if [[ $argnum -lt 1 ]]; then
-			echo -e "path to tgz backup files is not set."
+			echo "No path parameter."
 			exit 1
 		fi
 		restore $arg1 $arg2
